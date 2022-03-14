0573ef9add963ad0af4fd843b4a132e1b9c9fe35
laskor <al.moksud@gmail.com>
2022-03-14 01:13:59 -0400

    data added

--- js/app.js
+++ js/app.js
@@ -3,14 +3,24 @@ const API_KEY = `e2f02b2123c076151e918adf9e3d90c5`;
 
 const searchTemp = () => {
     const city = document.getElementById('search-input').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayTemp(data));
 }
-searchTemp();
 
 const displayTemp = temp => {
+    const divT = document.getElementById('divT');
+    divT.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
+            <h1>${temp.name}</h1>
+            <h3><span>${temp.main.temp}</span>&deg;C</h3>
+            <h2>country: ${temp.sys.country} </h2>
+            <h1 class="lead">${temp.weather[0].main}</h1>
+        `;
+        divT.appendChild(div);
+    
     
 }
