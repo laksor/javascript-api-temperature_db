9eb049f37f9d0a9ba117fab1f7bbf38617732949
laskor <al.moksud@gmail.com>
2022-03-14 01:27:38 -0400

    innertext

--- js/app.js
+++ js/app.js
@@ -22,17 +22,11 @@ const displayTemp = temp => {
         `;
         divT.appendChild(div);
 }
+// using inner text
+const setInnerText = (id, tempp) => {
+    document.getElementById(id).innerText = text;
+}
 
-// const displayTemp = temp => {
-//     const divT = document.getElementById('divT');
-//     divT.textContent = '';
-//         const div = document.createElement('div');
-//         div.innerHTML = `
-//             <img src="https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png" alt="">
-//             <h1>${temp.name}</h1>
-//             <h3><span>${temp.main.temp}</span>&deg;F</h3>
-//             <h2>country: ${temp.sys.country} </h2>
-//             <h1 class="lead">condition : ${temp.weather[0].main}</h1>
-//         `;
-//         divT.appendChild(div);
-// }
+const displayTempp = tempp => {
+    setInnerText('temperature', tempp.temp);
+}
