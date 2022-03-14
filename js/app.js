b370664d768e46a6539a65173a7977362be9252e
laskor <al.moksud@gmail.com>
2022-03-14 00:39:22 -0400

    api added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+const API_KEY = `e2f02b2123c076151e918adf9e3d90c5`;
+
+const searchTemp = () => {
+    const city = document.getElementById('search-input').value;
+    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
