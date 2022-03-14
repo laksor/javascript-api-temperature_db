5d89986bcdd4faf28919e2500ee13ab55a269b07
laskor <al.moksud@gmail.com>
2022-03-14 01:42:46 -0400

    project done

--- js/app.js
+++ js/app.js
@@ -13,17 +13,23 @@ const displayTemp = temp => {
     const divT = document.getElementById('divT');
     divT.textContent = '';
         const div = document.createElement('div');
+        div.classList.add('bg-light');
+        div.classList.add('py-5');
+        div.classList.add('text-dark');
+        div.classList.add('shadow-lg');
+        div.classList.add('rounded');
         div.innerHTML = `
             <img src="https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png" alt="">
             <h1>${temp.name}</h1>
             <h3><span>${temp.main.temp}</span>&deg;F</h3>
             <h2>country: ${temp.sys.country} </h2>
-            <h1 class="lead">condition : ${temp.weather[0].main}</h1>
+            <h3>condition : ${temp.weather[0].main}</h3>
         `;
         divT.appendChild(div);
 }
+
 // using inner text
-const setInnerText = (id, tempp) => {
+const setInnerText = (id, text) => {
     document.getElementById(id).innerText = text;
 }
 
@@ -31,9 +37,4 @@ const displayTempp = tempp => {
     setInnerText('city', tempp.name);
     setInnerText('temperature', tempp.main.temp);
     setInnerText('condition', tempp.weather[0].main);
-    setInnerText('temperature', tempp.temp);
-    setInnerText('temperature', tempp.temp);
-    setInnerText('temperature', tempp.temp);
-    setInnerText('temperature', tempp.temp);
-    setInnerText('temperature', tempp.temp);
 }
