7b2e99ac2fe10a3934e5a9e7975138f4c370bb1f
laskor <al.moksud@gmail.com>
2022-03-14 01:17:01 -0400

    condition added

--- js/app.js
+++ js/app.js
@@ -16,9 +16,9 @@ const displayTemp = temp => {
         div.innerHTML = `
             <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
             <h1>${temp.name}</h1>
-            <h3><span>${temp.main.temp}</span>&deg;C</h3>
+            <h3><span>${temp.main.temp}</span>&deg;F</h3>
             <h2>country: ${temp.sys.country} </h2>
-            <h1 class="lead">${temp.weather[0].main}</h1>
+            <h1 class="lead">condition : ${temp.weather[0].main}</h1>
         `;
         divT.appendChild(div);
     
