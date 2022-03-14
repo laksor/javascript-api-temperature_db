1bb06b079d3a53fac66238a658899a4df4a7b4ec
laskor <al.moksud@gmail.com>
2022-03-14 00:51:46 -0400

    bug fixed

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@ const API_KEY = `e2f02b2123c076151e918adf9e3d90c5`;
 
 const searchTemp = () => {
     const city = document.getElementById('search-input').value;
-    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => console.log(data));
+}
+searchTemp();
+
+const displayTemp = temp => {
+    
 }
